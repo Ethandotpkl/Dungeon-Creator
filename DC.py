4bc1372df1c41f2b62d75c57af887708bab491ec
Ethan Newburn <ethan.newburn@gmail.com>
2024-11-03 19:18:58 -0800

    Added gridlines and improved makeroom

--- DC.py
+++ DC.py
@@ -21,6 +21,7 @@ nextpos = ((mapwidth / 2) + 14, (mapheight / 2))
 #print(nextpos)
 shape_size = (chunksize, chunksize)
 shape_color = (255, 255, 255)
+grid_color = (130, 130, 130)
 
 # make a pygame.Rect variable for the shape:
 shape_rect = pygame.Rect(shapepos, shape_size)
@@ -62,21 +63,20 @@ def makeroom(DM, start_pos):
     #print(start_pos)
     DM[start_pos[0]][start_pos[1]] = 1
     cur_cell = start_pos
-    while cur_size < roomsize:
-        for i in range (-1, 1):
-            for j in range(-1, 1):
-                if i != 0 and j != 0:
-                    checkpos = (cur_cell[0] + i, cur_cell[1] + j)
-                    if cardinal_check(DM, checkpos, cur_cell) == True:
-                        #print("adding cell")
-                        DM[checkpos[0]][checkpos[1]] = 1
-                        cur_size += 1
-
+    #while cur_size < roomsize:
+    for i in range (-1, 2):
+            for j in range(-1, 2):
+                print(f"i: {i}  j: {j}")
+                if not (i == 0 and j == 0):
+                    DM[cur_cell[0] + i][cur_cell[1] + j] = 1
+                    print(f"i: {i}  j: {j}")
+                else:
+                    print("0, 0")
 makeroom(DM, (50, 50))
 #print(DM)
 rownum = 0
 for row in DM:
-        print(row)
+        #print(row)
         colnum = 0
         for element in row:
             if element == 1:
@@ -86,7 +86,9 @@ for row in DM:
                 #print(position)
                 room_fill = pygame.Rect(position, shape_size)
                 screen.fill(shape_color, rect=room_fill)
+            pygame.draw.line(screen, grid_color, (colnum * 7, 0), (colnum * 7, mapheight), 1)
             colnum += 1
+        pygame.draw.line(screen, grid_color, (0, rownum * 7), (mapwidth, rownum * 7), 1)
         rownum += 1
 
 while True:
@@ -95,7 +97,6 @@ while True:
         # The pygame.QUIT event happens when someone tries to close the game window.
         if event.type == pygame.QUIT:
             sys.exit()
-    
     #screen.fill(shape_color, rect=shape_rect)
     #screen.fill(shape_color, rect=shape_rect2)
     #pause = input(">>")
